77dd187fc4b69869ade565cf6463917bb45d25cd
ZAstafyeva <z.a.astafyeva@gmail.com>
2016-08-26 21:40:21 +1000

    Separate model data

--- app.js
+++ app.js
@@ -1,74 +1,3 @@
-//  Photo attribution:
-//  http://lisadorenfest.com/
-//  http://graffitisdumonde.blogspot.com.au/
-
-var artLocations = [
-  {
-    author: "DEAMS, Melbourne",
-    artworkLocation: "61 Parry Street, Newcastle West"
-  },{
-    author: "WBYK, Sydney",
-    artworkLocation: "81 Parry Street, Newcastle West"
-  },{
-    author: "Alex Lehours, Sydney",
-    artworkLocation: "590 Hunter Street, Newcastle West"
-  },{
-    author: "TWOONE, Melbourne",
-    artworkLocation: "426 King Street, Newcastle West"
-  },{
-    author: "Numskull & Adnate",
-    artworkLocation: "291 King St, Newcastle"
-  },{
-    author: "Thomas Jackson, Sydney",
-    artworkLocation: "44 Laman street, Cooks Hill"
-  },{
-    author: "Dan Prestage",
-    artworkLocation: "500 Hunter street, Newcastle"
-  },{
-    author: "BMD, NZ",
-    artworkLocation: "24 Dawson street, Cooks Hill"
-  },{
-    author: "Shannon Crees, Sydney",
-    artworkLocation: "85 Darby Street, Cooks Hill"
-  },{
-    author: "Nico, Sydney",
-    artworkLocation: "70 Darby Street, Cooks Hill"
-  },{
-    author: "Tristan Eaton, USA",
-    artworkLocation: "113-125 Darby Street, Cooks Hill"
-  },{
-    author: "Jumbo & Bafcat",
-    artworkLocation: "5 Railway Street, Merewether"
-  },{
-    author: "Jumbo, Sydney",
-    artworkLocation: "223 King Street, Newcastle"
-  },{
-    author: "Askew, Auckland",
-    artworkLocation: "Crowne Plaza, Honeysuckle"
-  },{
-    author: "Adnate, Melbourne",
-    artworkLocation: "2 Bishopsgate Street, Wickham"
-  },{
-    author: "Tyrsa, France",
-    artworkLocation: "707 Hunter Street, Newcastle"
-  },{
-    author: "Slicer & Lucy Lucy & Deams",
-    artworkLocation: "286 Maitland Rd, Mayfield"
-  },{
-    author: "Chehehe, Melbourne",
-    artworkLocation: "180 Maitland Road, Mayfield"
-  },{
-    author: "Ox King & SMC3",
-    artworkLocation: "156 Maitland Rd, Mayfield"
-  },{
-    author: "Grizzle & Skulk",
-    artworkLocation: "9 Albert Street, Wickham"
-  },{
-    author: "Adnate, Melbourne",
-    artworkLocation: "Bolton street, Newcastle"
-  }
-];
-
 function AppViewModel() {
   var self = this;
   var markers = [];
@@ -77,10 +6,10 @@ function AppViewModel() {
 
 
   function initializeMap() {
-    newcastle = new google.maps.latLng(-32.929927, 151.773169);
+    newcastle = new google.maps.LatLng(-32.929927, 151.773169);
     map = new google.maps.Map(document.getElementById('map'), {
         center: newcastle,
-        zoom: 16,
+        zoom: 15,
         mapTypeControl: false
     });
   }
---
+++ model.js
@@ -0,0 +1,91 @@
+//  Photo attribution:
+//  http://lisadorenfest.com/
+//  http://graffitisdumonde.blogspot.com.au/
+
+var artLocations = [
+  {
+    author: "DEAMS, Melbourne",
+    artworkLocation: "61 Parry Street, Newcastle West",
+    coordinates: {lat: -32.927980, lng: 151.759358}
+  },{
+    author: "WBYK, Sydney",
+    artworkLocation: "81 Parry Street, Newcastle West",
+    coordinates: {lat: -32.927392, lng: 151.758489}
+  },{
+    author: "Alex Lehours, Sydney",
+    artworkLocation: "590 Hunter Street, Newcastle West",
+    coordinates: {lat: -32.926667, lng: 151.766173}
+  },{
+    author: "TWOONE, Melbourne",
+    artworkLocation: "426 King Street, Newcastle West",
+    coordinates: {lat: -32.927487, lng: 151.765223}
+  },{
+    author: "Numskull & Adnate",
+    artworkLocation: "291 King St, Newcastle",
+    coordinates: {lat: -32.928587, lng: 151.768000}
+  },{
+    author: "Thomas Jackson, Sydney",
+    artworkLocation: "44 Laman street, Cooks Hill",
+    coordinates: {lat: -32.929034, lng: 151.769969}
+  },{
+    author: "Dan Prestage",
+    artworkLocation: "500 Hunter street, Newcastle",
+    coordinates: {lat: -32.926760, lng: 151.769852}
+  },{
+    author: "BMD, NZ",
+    artworkLocation: "24 Dawson street, Cooks Hill",
+    coordinates: {lat: -32.930269, lng: 151.770939}
+  },{
+    author: "Shannon Crees, Sydney",
+    artworkLocation: "85 Darby Street, Cooks Hill",
+    coordinates: {lat: -32.930275, lng: 151.772649}
+  },{
+    author: "Nico, Sydney",
+    artworkLocation: "70 Darby Street, Cooks Hill",
+    coordinates: {lat: -32.929708, lng: 151.772818}
+  },{
+    author: "Tristan Eaton, USA",
+    artworkLocation: "113-125 Darby Street, Cooks Hill",
+    coordinates: {lat: -32.931212, lng: 151.772216}
+  },{
+    author: "Jumbo & Bafcat",
+    artworkLocation: "5 Railway Street, Merewether",
+    coordinates: {lat: -32.922152, lng: 151.756292}
+  },{
+    author: "Jumbo, Sydney",
+    artworkLocation: "223 King Street, Newcastle",
+    coordinates: {lat: -32.927992, lng: 151.770814}
+  },{
+    author: "Askew, Auckland",
+    artworkLocation: "Crowne Plaza, Honeysuckle",
+    coordinates: {lat: -32.925622, lng: 151.773055}
+  },{
+    author: "Adnate, Melbourne",
+    artworkLocation: "2 Bishopsgate Street, Wickham",
+    coordinates: {lat: -32.922804, lng: 151.760176}
+  },{
+    author: "Tyrsa, France",
+    artworkLocation: "707 Hunter Street, Newcastle",
+    coordinates: {lat: -32.926248, lng: 151.761640}
+  },{
+    author: "Slicer & Lucy Lucy & Deams",
+    artworkLocation: "286 Maitland Rd, Mayfield",
+    coordinates: {lat: -32.896806, lng: 151.735608}
+  },{
+    author: "Chehehe, Melbourne",
+    artworkLocation: "180 Maitland Road, Mayfield",
+    coordinates: {lat: -32.900497, lng: 151.740542}
+  },{
+    author: "Ox King & SMC3",
+    artworkLocation: "156 Maitland Rd, Mayfield",
+    coordinates: {lat: -32.901225, lng: 151.741271}
+  },{
+    author: "Grizzle & Skulk",
+    artworkLocation: "9 Albert Street, Wickham",
+    coordinates: {lat: -32.918106, lng: 151.758055}
+  },{
+    author: "Adnate, Melbourne",
+    artworkLocation: "Bolton street, Newcastle",
+    coordinates: {lat: -32.929286, lng: 151.782625}
+  }
+];
